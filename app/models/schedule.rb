fe2e356d9089b79c941b4530680c1e4b609c9733
mn <eaglel4rk@gmail.com>
2025-03-11 23:51:32 +0900

    バリデーション設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,7 @@
 class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true, comparison: { greater_than_or_equal_to: :start_date }
+  validates :all_day, presence: true
+  validates :memo, length: { maximum: 200 }
 end
